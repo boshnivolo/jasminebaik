d9db1a8c72961b4f48345f356ad1e525e588f5a7
Jasmine Baik <boshnivolo@gmail.com>
2016-03-18 15:58:43 -0400

    Moved controller into the module to troubleshoot

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,13 @@
     })
 
   }) // END .when routing
+
+  .controller('ListListingController', ['$scope', '$http', function($scope, $http){
+      //Server call to get listings
+      $http.get("https://openapi.etsy.com/v2/listings/active?api_key=hr9qkeabvt5jn57akxmxzm0o")
+        .then(function(response){
+          $scope.listings = response.data;
+        })
+  }]);
+
 })(); //END IIFE
