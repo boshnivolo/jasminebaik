bbfc320601a7dcbab257df0f03fa9b06a4ea2c5e
Jasmine Baik <boshnivolo@gmail.com>
2016-03-18 13:54:55 -0400

    Attempt at chrome extension settings

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
-$.getJSON("https://openapi.etsy.com/v2/users/active?api_key=hr9qkeabvt5jn57akxmxzm0o", function(data) {
-  console.log(data.results.listing_id);
-});
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+  $.getJSON("https://openapi.etsy.com/v2/users/active?api_key=hr9qkeabvt5jn57akxmxzm0o", function(data) {
+    console.log(data.results.listing_id);
+  });
+};
