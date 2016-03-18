76f6e6bb8b7c681ab67166d739b0eac285719142
Jasmine Baik <boshnivolo@gmail.com>
2016-03-18 12:02:37 -0400

    Alter Etsy params

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,3 @@
-$.getJSON("https://openapi.etsy.com/v2/listings/active?api_key=hr9qkeabvt5jn57akxmxzm0o", function(data) {
-  console.log(data);
+$.getJSON("https://openapi.etsy.com/v2/users/active?api_key=hr9qkeabvt5jn57akxmxzm0o", function(data) {
+  console.log(data.results.listing_id);
 });
